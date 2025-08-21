7660f72d4f25da0e744f83a92679bce7d04e66f3
PavanBollepalli <pavanvenkatanagamanoj@gmail.com>
2025-08-21 20:31:01 +0530

    connected to mongodb

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ import express from "express";
 import { PORT } from "./config/env.js";
 import { authRouter } from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
-import subscriptionRouter from "./subscription.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express()
 
@@ -14,8 +15,9 @@ app.get("/", (req, res) => {
     res.send("Welcome to Subscription Tracker API")
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Subscription Tracker Live at http://localhost:${PORT}`)
+    await connectToDatabase()
 })
 
 export default app
---
+++ models/user.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const UserSchema =new mongoose.Schema({
+    name:{
+        type:String,
+        trim:true,
+        required:true,
+        minlength:2,
+        maxlength:20,
+    },
+    email:{
+        type:String,
+        required:true,
+        lowerCase:true,
+        match:[/\S+@\S+\.\S+/,"please fill a valid email address"]
+    }
+})
+const User=mongoose.model("User",UserSchema);
+export default User;
